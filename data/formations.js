927a568548d893e87ae4cad884839e46c86e7e0f
ibrahima <diaibou29@gmail.com>
2022-06-27 15:34:05 +0200

    first commit

---
+++ data/formations.js
@@ -0,0 +1,43 @@
+let formations = [{
+        nom: "Bachelor bac+2 -Devops",
+        date: "En COURS ",
+        ecole: "EPSI, Lyon.",
+        description: "Algorithmie C#, C++, JavaScript Développement WEB (Framework Symfony) Linux, architecture réseau..."
+    },
+    {
+        nom: "GIT & GITHUB - INFORMATIQUE",
+        date: "Oct. 2021",
+        ecole: "Open Classeroom.",
+        description: " "
+
+    },
+    {
+        nom: "PARCOURS FAST TRACK IT & DIGITAL",
+        date: "Juil. 2021",
+        ecole: "EPSI, Lyon.",
+        description: " apprentissage du développement WEB (HTML, CSS) Algorithmie (Python) Marketing(SEO, Acquisition de LEADS...)"
+
+    },
+    {
+        nom: "HTLM, CSS & JAVA SCRIPT POUR LE WEB ET API - WEB",
+        date: "Juin 2021",
+        ecole: "Open Classeroom.",
+        description: " "
+
+    },
+    {
+        nom: "SWIFT - ALGORITHME",
+        date: "Juil 2022",
+        ecole: "Simplon",
+        description: " "
+
+    },
+    {
+        nom: "Prepa Mathématiques",
+        date: "Sept. 2018",
+        ecole: "ESP, Prépa maths, Dakar.",
+        description: ""
+
+    }
+
+]
---
+++ script/projects.js
@@ -0,0 +1,39 @@
+class project {
+
+    constructor(id, begin, type, status, description, link, img) {
+        this.id = id;
+        this.begin = begin;
+        this.type = type;
+        this.status = status;
+        this.description = description;
+        this.link = link;
+        this.img = img;
+    }
+
+}
+
+
+let projects = [
+    new project("Serveur Minecraft sur Raspberry Pi", "09-2021", "linux, Servuer WEB", "fini", "redirection de port sur une box pour avoir un serveur minecaft sur le raspberry PI", "", "img/tech.jpg"),
+
+    new project("site meteo RT", "08-2021", "WEB, API", "fini(version 1)", "un site de qui vous affiche la météo en fonctionde votre ville", '<a href="http://datarayan.6te.net/" class="btn btn-outline-dark">tester</a>', "img/web.jpg"),
+
+    new project("meteo avec synthese vocal", "08-2021", "python, API", "fini", "une meteo avec fonction vocal", "", "img/dev.jpg"),
+
+    new project("Calculatrice pour Polynomes", "06-2021", "JavaScript, nodeJS", "en cours", "calculatrice pour calculer les dérivée, les primitives ...", "", "img/dev.jpg"),
+
+    new project("site WEB e-learning - ENM", "04-2021", "en stage", "WEB , front-end, backend", "site web d'apprentissage ...", "", "img/web.jpg")
+]
+
+
+
+let p = document.getElementById("pro")
+
+let prjs = projects
+
+function onLoadProject() {
+    for (let i = 0; i < prjs.length; i++) {
+        p.innerHTML += '<div class="card my-3" ><img src="' + prjs[i].img + '" class="card-img-top" alt="..."><div class="card-body"><div class="d-flex justify-content-between"><h5 class="card-title">' + prjs[i].id + '</h5> <i class="bi bi-three-dots-vertical" data-bs-toggle="collapse" data-bs-target="#collapseCard" aria-expanded="false" aria-controls="collapseCard"></i></div><div class="collapse" id="collapseCard"><p class="card-text">' + prjs[i].description + '</p><p class="card-text">' + prjs[i].begin + '</p><div class="d-flex">' + prjs[i].link + '</div></div></div></div>';
+
+    }
+}
